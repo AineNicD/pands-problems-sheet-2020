0cf44eccc7b18591ae2649f5d47f4a871e050462
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-01-30 14:21:04 +0000

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,21 @@
+#This program calculates somebody's Body Mass Index (BMI).  
+#The user inputs the person's height in centimetres and weight in kilograms. 
+#The output is their weight divided by their height in metres squared. 
+
+#height will store the value inputed by the user.
+height = float(input("Enter persons height in cm: "))
+
+#weight will store the value inputed by the user.
+weight = float(input("Enter persons weight in kg: "))
+
+#Bmi calculation
+bmi = weight / ((height/100)**2)
+
+#Rounding calculation to 2 decimal places
+decimal_places = (round(bmi,2))
+
+print("----------------------------------")
+
+print("Body Mass Index is : ",decimal_places)
+
+print("----------------------------------")
