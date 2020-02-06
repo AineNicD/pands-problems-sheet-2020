7246d97b3bd1e8eecca3ae84b2a2715afd5ca69c
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-02-06 17:54:40 +0000

    Update lab3.2-sub.py

--- Labs/lab3.2-sub.py
+++ Labs/lab3.2-sub.py
@@ -1,1 +1,14 @@
+#Write a program that reads	in	two	numbers	and	
+# subracts the first one from the second one. 
 
+#Program to substract one number from another.
+# input reads out a string so we convert it into an int
+# so we can perform mathematical operations.
+
+x = int(input( "Enter first number:"))
+
+y = int(input( "Enter second number:"))
+
+answer = x - y
+
+print( "{} minus {} is {} " .format(x, y, answer))
