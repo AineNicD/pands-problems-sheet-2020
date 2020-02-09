4537b0fb5d8914053e6c4b2dcf32cc922c1aad43
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-02-09 00:57:22 +0000

    Grade.py

--- Labs/Topic04-Flow/lab04.01-grade.py
+++ Labs/Topic04-Flow/lab04.01-grade.py
@@ -1,1 +1,18 @@
+# This program reads in a students percentage
+# and prints out the corresponding grade
 
+percentage = float(input("Enter the percentage: "))
+#print (percentage)
+
+if percentage  < 0 or percentage > 100:
+    print ("Please enter a mu,ner between 0 and 100")
+elif percentage < 40:# we know it is greater than 0
+    print ("Fail")
+elif percentage < 50: #between 40 and 49
+    print ("Pass")
+elif percentage < 60: # between 50 and 59
+    print ("Merit1")
+elif percentage < 70: # between 60 and 69
+    print ("Merit2") 
+else: #the only option left is between 70 and 100
+    print ("Distinction")
