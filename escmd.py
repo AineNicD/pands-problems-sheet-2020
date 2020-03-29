f0e71329540a18988f2f97ffddc78e6b0333acac
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-03-30 00:54:03 +0100

    spelling corrections and tidy up

--- escmd.py
+++ escmd.py
@@ -1,16 +1,17 @@
 #Aine Nic Dhonnacha
 #Weekly task 7
-#Write a program that reads in a text file and outputs the number of e's it contains.
-#The program should take the filename from an argument on the command line.
+#A program that reads in a text file and outputs the number of e's it contains.
+#The program takes the filename from an argument on the command line.
 #$ python es.py moby-dick.txt
 #116960
 
 import sys
 
 #from command line 
-#program reads a file call mobydick.txt and outputs the number of e's in the document
-#read in UTF-8 which the mobydick text is in(code was producing error at a character in the program)
+#program reads a file called mobydick.txt and outputs the number of e's in the document
+#read in UTF-8 which the mobydick text is in(previous code was producing an error at a character in the program)
 #Unicode has more characters. 
+
 with open(sys.argv[1], encoding="utf8") as f:
   text = f.read()
   countE = text.count("e")
@@ -19,7 +20,7 @@ with open(sys.argv[1], encoding="utf8") as f:
 
 
 
-#ref
+#references;
 #https://realpython.com/read-write-files-python/
 #https://www.w3schools.com/python/ref_string_count.asp 
 #https://www.datacamp.com/community/tutorials/exception-handling-python
