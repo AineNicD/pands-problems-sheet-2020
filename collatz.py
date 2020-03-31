09a4db03e5d977172dc9a1e6ea6c2f0b0004887d
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-03-31 23:17:27 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,11 +1,12 @@
+#Aine Nic Dhonnacha
 #Weekly task 4
 
-#Write a program that asks the user to input any positive integer 
+#A program that asks the user to input any positive integer 
 #and outputs the successive values of the following calculation. 
 #At each step calculate the next value by taking the current value and, 
 #if it is even, divide it by two, but if it is odd, 
 #multiply it by three and add one.
-#Have the program end if the current value is one.
+#The program will end if the current value is one.
 
 #$ python collatz.py
 #Please enter a positive integer: 10
@@ -32,6 +33,7 @@ else:
 
 
 
+#references;
 #https://www.programiz.com/python-programming/examples/odd-even
 #https://docs.python.org/3/tutorial/introduction.html#numbers
 #https://docs.python.org/3/tutorial/controlflow.html#if-statements
