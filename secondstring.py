1affd5f8c457169c73f00c518e6c28f3dcc2ba10
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-03-31 23:24:07 +0100

    Update secondstring.py

--- secondstring.py
+++ secondstring.py
@@ -1,8 +1,9 @@
 #Aine Nic Dhonnacha
 
 # Weekly task 3
-# Write a program that takes asks a user to input a string
+# A program that asks a user to input a string
 # and outputs every second letter in reverse order. 
+
 # $ python secondstring.py
 # Please enter a sentence: The quick brown fox jumps over the lazy dog.
 # .o zletrv pu o wr cu h
