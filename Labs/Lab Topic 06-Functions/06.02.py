665baa5904b510c4d04bbf946438a328023378e2
AineNicD <59982405+AineNicD@users.noreply.github.com>
2020-04-03 23:17:05 +0100

    06.02.py

---
+++ Labs/Lab Topic 06-Functions/06.02.py
@@ -0,0 +1,23 @@
+#Write a function that prints out a menu of commands we can perform, 
+#ie add, view and quit. The function should return what the user chose. 
+#Test the function. We don’t need to worry about error handling yet 
+
+#What would you like to do?         
+#(a) Add new student         
+#(v) View students         
+#(q) Quit 
+#Type one letter (a/v/q):d 
+#you chose d
+
+def displayMenu():     
+    print("What would you like to do?")     
+    print("\t(a) Add new student")     
+    print("\t(v) View students")     
+    print("\t(q) Quit")     
+    choice = input("Type one letter (a/v/q):").strip() 
+    return choice
+    #test the function 
+choice = displayMenu() 
+print("you chose {}".format(choice)
+
+#Reference: Andrew Beatty Lab Topic 06-functions
